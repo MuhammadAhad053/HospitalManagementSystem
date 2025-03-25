9a590c7489cf5c2a9e0ae5130f978ff0a8b4ee6b
M-Danish-Meraj <muhammaddanishmeraj@gmail.com>
2025-03-26 00:15:10 +0500

    starting point reset to signup page

--- modernsigninUI/Program.cpp
+++ modernsigninUI/Program.cpp
@@ -14,37 +14,26 @@ void main(array<String^>^ args) {
 	Application::SetCompatibleTextRenderingDefault(false);
 
 
-	Receptionist^ res1 = gcnew Receptionist;
-	res1->id = 1;
-	res1->firstName = "John";
-	res1->lastName = "Doe";
-	res1->gender = "male";
-	res1->dateofBirth = "1999-01-01";
-	res1->phoneNumber = "1234567890";
-	res1->email = "aaaa";
-	HospitalManagement::ReceptionistDashboard^receptionistDashboard = gcnew HospitalManagement::ReceptionistDashboard(res1);
-	receptionistDashboard->ShowDialog();
-}
-//
-//	Patient^ patient = nullptr;	
-//
-//	while (true) {
-//		HospitalManagement::LoginForm loginForm;	
-//		loginForm.ShowDialog();
-//		if (loginForm.switchtoRegister) {
-//			HospitalManagement::RegisterForm registerForm;
-//			registerForm.ShowDialog();
-//			if (registerForm.switchtoLogin) {
-//				continue;
-//			}
-//			else {
-//				patient = registerForm.patient;
-//				break;
-//			}
-//		}
-//		else {
-//			patient = loginForm.patient;
-//			break;
-//		}
-//	}
-//}
+
+	Patient^ patient = nullptr;	
+
+	while (true) {
+		HospitalManagement::LoginForm loginForm;	
+		loginForm.ShowDialog();
+		if (loginForm.switchtoRegister) {
+			HospitalManagement::RegisterForm registerForm;
+			registerForm.ShowDialog();
+			if (registerForm.switchtoLogin) {
+				continue;
+			}
+			else {
+				patient = registerForm.patient;
+				break;
+			}
+		}
+		else {
+			patient = loginForm.patient;
+			break;
+		}
+	}
+	}
