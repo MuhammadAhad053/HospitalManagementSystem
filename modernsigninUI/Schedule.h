370a2544c020e8b8927877c8a85accab03d5ab21
MuhammadAhad053 <ahadmuhammad053@gmail.com>
2025-04-22 20:18:00 +0500

    added schedule form

---
+++ modernsigninUI/Schedule.h
@@ -0,0 +1,300 @@
+#pragma once
+
+namespace HospitalManagement {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for Schedule
+	/// </summary>
+	public ref class Schedule : public System::Windows::Forms::Form
+	{
+	public:
+		Schedule(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~Schedule()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Panel^ panel1;
+	private: System::Windows::Forms::Panel^ panel2;
+	private: System::Windows::Forms::Label^ label9;
+	private: System::Windows::Forms::Button^ btProfile;
+	private: System::Windows::Forms::Label^ label8;
+
+
+	private: System::Windows::Forms::Label^ lbAmount;
+	private: System::Windows::Forms::Button^ btnTransfer;
+	private: System::Windows::Forms::Panel^ panel4;
+	private: System::Windows::Forms::TextBox^ tbAccount;
+	private: System::Windows::Forms::Label^ label6;
+
+
+	private: System::Windows::Forms::Label^ label5;
+	private: System::Windows::Forms::PictureBox^ pictureBox2;
+	private: System::Windows::Forms::Label^ label4;
+
+	protected:
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(Schedule::typeid));
+			this->panel1 = (gcnew System::Windows::Forms::Panel());
+			this->panel2 = (gcnew System::Windows::Forms::Panel());
+			this->label9 = (gcnew System::Windows::Forms::Label());
+			this->btProfile = (gcnew System::Windows::Forms::Button());
+			this->label8 = (gcnew System::Windows::Forms::Label());
+			this->lbAmount = (gcnew System::Windows::Forms::Label());
+			this->btnTransfer = (gcnew System::Windows::Forms::Button());
+			this->panel4 = (gcnew System::Windows::Forms::Panel());
+			this->tbAccount = (gcnew System::Windows::Forms::TextBox());
+			this->label6 = (gcnew System::Windows::Forms::Label());
+			this->label5 = (gcnew System::Windows::Forms::Label());
+			this->pictureBox2 = (gcnew System::Windows::Forms::PictureBox());
+			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->panel1->SuspendLayout();
+			this->panel2->SuspendLayout();
+			this->panel4->SuspendLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox2))->BeginInit();
+			this->SuspendLayout();
+			// 
+			// panel1
+			// 
+			this->panel1->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(15)), static_cast<System::Int32>(static_cast<System::Byte>(50)),
+				static_cast<System::Int32>(static_cast<System::Byte>(69)));
+			this->panel1->BackgroundImageLayout = System::Windows::Forms::ImageLayout::None;
+			this->panel1->Controls->Add(this->pictureBox2);
+			this->panel1->Controls->Add(this->label4);
+			this->panel1->Controls->Add(this->btProfile);
+			this->panel1->Controls->Add(this->label9);
+			this->panel1->Dock = System::Windows::Forms::DockStyle::Top;
+			this->panel1->Location = System::Drawing::Point(0, 0);
+			this->panel1->Name = L"panel1";
+			this->panel1->Size = System::Drawing::Size(1264, 70);
+			this->panel1->TabIndex = 0;
+			// 
+			// panel2
+			// 
+			this->panel2->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"panel2.BackgroundImage")));
+			this->panel2->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Stretch;
+			this->panel2->Controls->Add(this->btnTransfer);
+			this->panel2->Controls->Add(this->panel4);
+			this->panel2->Controls->Add(this->label6);
+			this->panel2->Controls->Add(this->label5);
+			this->panel2->Controls->Add(this->lbAmount);
+			this->panel2->Controls->Add(this->label8);
+			this->panel2->Dock = System::Windows::Forms::DockStyle::Fill;
+			this->panel2->Location = System::Drawing::Point(0, 70);
+			this->panel2->Name = L"panel2";
+			this->panel2->Size = System::Drawing::Size(1264, 611);
+			this->panel2->TabIndex = 1;
+			// 
+			// label9
+			// 
+			this->label9->AutoSize = true;
+			this->label9->BackColor = System::Drawing::Color::Transparent;
+			this->label9->Font = (gcnew System::Drawing::Font(L"Segoe UI Variable Display", 18.25F, System::Drawing::FontStyle::Bold));
+			this->label9->ForeColor = System::Drawing::Color::White;
+			this->label9->Location = System::Drawing::Point(553, 22);
+			this->label9->Name = L"label9";
+			this->label9->Size = System::Drawing::Size(124, 33);
+			this->label9->TabIndex = 16;
+			this->label9->Text = L"Schedule";
+			this->label9->TextAlign = System::Drawing::ContentAlignment::MiddleCenter;
+			// 
+			// btProfile
+			// 
+			this->btProfile->BackColor = System::Drawing::Color::Transparent;
+			this->btProfile->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"btProfile.BackgroundImage")));
+			this->btProfile->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Zoom;
+			this->btProfile->FlatAppearance->BorderColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(20)),
+				static_cast<System::Int32>(static_cast<System::Byte>(55)), static_cast<System::Int32>(static_cast<System::Byte>(64)));
+			this->btProfile->FlatAppearance->MouseDownBackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(25)),
+				static_cast<System::Int32>(static_cast<System::Byte>(60)), static_cast<System::Int32>(static_cast<System::Byte>(79)));
+			this->btProfile->FlatAppearance->MouseOverBackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(20)),
+				static_cast<System::Int32>(static_cast<System::Byte>(55)), static_cast<System::Int32>(static_cast<System::Byte>(64)));
+			this->btProfile->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->btProfile->Location = System::Drawing::Point(12, 12);
+			this->btProfile->Name = L"btProfile";
+			this->btProfile->Size = System::Drawing::Size(55, 50);
+			this->btProfile->TabIndex = 17;
+			this->btProfile->UseVisualStyleBackColor = false;
+			// 
+			// label8
+			// 
+			this->label8->AutoSize = true;
+			this->label8->BackColor = System::Drawing::Color::Transparent;
+			this->label8->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->label8->Font = (gcnew System::Drawing::Font(L"Segoe UI Variable Display", 36, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label8->ForeColor = System::Drawing::SystemColors::ControlLight;
+			this->label8->Location = System::Drawing::Point(148, 206);
+			this->label8->Name = L"label8";
+			this->label8->Size = System::Drawing::Size(349, 64);
+			this->label8->TabIndex = 19;
+			this->label8->Text = L"Your Schedule";
+			this->label8->TextAlign = System::Drawing::ContentAlignment::MiddleLeft;
+			// 
+			// lbAmount
+			// 
+			this->lbAmount->BackColor = System::Drawing::Color::Transparent;
+			this->lbAmount->Font = (gcnew System::Drawing::Font(L"Segoe UI Variable Display", 48, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->lbAmount->ForeColor = System::Drawing::SystemColors::ControlLight;
+			this->lbAmount->Location = System::Drawing::Point(128, 270);
+			this->lbAmount->Name = L"lbAmount";
+			this->lbAmount->Size = System::Drawing::Size(398, 85);
+			this->lbAmount->TabIndex = 5;
+			this->lbAmount->Text = L"10am - 4pm";
+			this->lbAmount->TextAlign = System::Drawing::ContentAlignment::MiddleCenter;
+			// 
+			// btnTransfer
+			// 
+			this->btnTransfer->BackColor = System::Drawing::Color::Transparent;
+			this->btnTransfer->FlatAppearance->BorderColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(20)),
+				static_cast<System::Int32>(static_cast<System::Byte>(55)), static_cast<System::Int32>(static_cast<System::Byte>(74)));
+			this->btnTransfer->FlatAppearance->BorderSize = 0;
+			this->btnTransfer->FlatAppearance->MouseDownBackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(25)),
+				static_cast<System::Int32>(static_cast<System::Byte>(60)), static_cast<System::Int32>(static_cast<System::Byte>(79)));
+			this->btnTransfer->FlatAppearance->MouseOverBackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(20)),
+				static_cast<System::Int32>(static_cast<System::Byte>(55)), static_cast<System::Int32>(static_cast<System::Byte>(74)));
+			this->btnTransfer->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->btnTransfer->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 14.25F));
+			this->btnTransfer->ForeColor = System::Drawing::Color::White;
+			this->btnTransfer->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"btnTransfer.Image")));
+			this->btnTransfer->ImageAlign = System::Drawing::ContentAlignment::MiddleRight;
+			this->btnTransfer->Location = System::Drawing::Point(921, 341);
+			this->btnTransfer->Name = L"btnTransfer";
+			this->btnTransfer->Size = System::Drawing::Size(134, 46);
+			this->btnTransfer->TabIndex = 22;
+			this->btnTransfer->Text = L"Change";
+			this->btnTransfer->TextAlign = System::Drawing::ContentAlignment::MiddleLeft;
+			this->btnTransfer->UseVisualStyleBackColor = false;
+			// 
+			// panel4
+			// 
+			this->panel4->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(15)), static_cast<System::Int32>(static_cast<System::Byte>(50)),
+				static_cast<System::Int32>(static_cast<System::Byte>(69)));
+			this->panel4->Controls->Add(this->tbAccount);
+			this->panel4->Location = System::Drawing::Point(713, 285);
+			this->panel4->Name = L"panel4";
+			this->panel4->Size = System::Drawing::Size(342, 35);
+			this->panel4->TabIndex = 25;
+			// 
+			// tbAccount
+			// 
+			this->tbAccount->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(15)), static_cast<System::Int32>(static_cast<System::Byte>(50)),
+				static_cast<System::Int32>(static_cast<System::Byte>(69)));
+			this->tbAccount->BorderStyle = System::Windows::Forms::BorderStyle::None;
+			this->tbAccount->Font = (gcnew System::Drawing::Font(L"Segoe UI Variable Display", 12.25F));
+			this->tbAccount->ForeColor = System::Drawing::Color::Silver;
+			this->tbAccount->Location = System::Drawing::Point(12, 5);
+			this->tbAccount->Name = L"tbAccount";
+			this->tbAccount->Size = System::Drawing::Size(292, 22);
+			this->tbAccount->TabIndex = 0;
+			this->tbAccount->Text = L"Time (eg 10am - 4pm)";
+			// 
+			// label6
+			// 
+			this->label6->AutoSize = true;
+			this->label6->BackColor = System::Drawing::Color::Transparent;
+			this->label6->Font = (gcnew System::Drawing::Font(L"Segoe UI Variable Display", 14.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label6->ForeColor = System::Drawing::SystemColors::ControlLight;
+			this->label6->Location = System::Drawing::Point(708, 253);
+			this->label6->Name = L"label6";
+			this->label6->Size = System::Drawing::Size(227, 26);
+			this->label6->TabIndex = 24;
+			this->label6->Text = L"Enter your new schedule";
+			this->label6->TextAlign = System::Drawing::ContentAlignment::MiddleLeft;
+			// 
+			// label5
+			// 
+			this->label5->AutoSize = true;
+			this->label5->BackColor = System::Drawing::Color::Transparent;
+			this->label5->Font = (gcnew System::Drawing::Font(L"Segoe UI Variable Display", 26.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label5->ForeColor = System::Drawing::SystemColors::ControlLight;
+			this->label5->Location = System::Drawing::Point(705, 188);
+			this->label5->Name = L"label5";
+			this->label5->Size = System::Drawing::Size(391, 47);
+			this->label5->TabIndex = 21;
+			this->label5->Text = L"Change your Schedule";
+			this->label5->TextAlign = System::Drawing::ContentAlignment::MiddleLeft;
+			// 
+			// pictureBox2
+			// 
+			this->pictureBox2->BackColor = System::Drawing::Color::Transparent;
+			this->pictureBox2->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox2.BackgroundImage")));
+			this->pictureBox2->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Zoom;
+			this->pictureBox2->Location = System::Drawing::Point(1194, 12);
+			this->pictureBox2->Name = L"pictureBox2";
+			this->pictureBox2->Size = System::Drawing::Size(58, 45);
+			this->pictureBox2->SizeMode = System::Windows::Forms::PictureBoxSizeMode::Zoom;
+			this->pictureBox2->TabIndex = 27;
+			this->pictureBox2->TabStop = false;
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->BackColor = System::Drawing::Color::Transparent;
+			this->label4->Font = (gcnew System::Drawing::Font(L"Segoe UI Variable Text Light", 10));
+			this->label4->ForeColor = System::Drawing::SystemColors::ControlLight;
+			this->label4->Location = System::Drawing::Point(1162, 38);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(37, 19);
+			this->label4->TabIndex = 26;
+			this->label4->Text = L"HMS\r\n";
+			this->label4->TextAlign = System::Drawing::ContentAlignment::MiddleLeft;
+			// 
+			// Schedule
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(1264, 681);
+			this->Controls->Add(this->panel2);
+			this->Controls->Add(this->panel1);
+			this->Name = L"Schedule";
+			this->Text = L"Schedule";
+			this->panel1->ResumeLayout(false);
+			this->panel1->PerformLayout();
+			this->panel2->ResumeLayout(false);
+			this->panel2->PerformLayout();
+			this->panel4->ResumeLayout(false);
+			this->panel4->PerformLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox2))->EndInit();
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	};
+}
